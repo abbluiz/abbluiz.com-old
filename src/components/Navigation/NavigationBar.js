7c9e4c1c2b0e19c64fbe02f8c7314d6000fa700c
LABB <l.a.b.barbosa@gmail.com>
2018-04-06 00:49:16 -0300

    Remove old material-ui code

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -1,41 +1,12 @@
 import React, { Component } from 'react';
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import IconButton from 'material-ui/IconButton';
-import NavigationMenuIcon from 'material-ui/svg-icons/navigation/menu';
-import NavigationOptions from './NavigationOptions';
-
 class NavigationBar extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
-  }
-
-  handleDrawerToggle = () => this.setState({open: !this.state.open});
-
-  handleDrawerClose = () => this.setState({open: false});
 
   render () {
 
     return (
 
       <div>
-        <AppBar
-          title="LABB"
-          iconElementLeft={<IconButton onClick={this.handleDrawerToggle}><NavigationMenuIcon /></IconButton>}
-          iconElementRight={<NavigationOptions />}
-        />
-        <Drawer
-          docked={false}
-          width={200}
-          open={this.state.open}
-          onRequestChange={(open) => this.setState({open})}
-        >
-          <MenuItem onClick={this.handleDrawerClose}>Cool Link</MenuItem>
-          <MenuItem onClick={this.handleDrawerClose}>Another One</MenuItem>
-        </Drawer>
+        NavigationBar.js
       </div>
 
     )
--- src/components/Site.js
+++ src/components/Site.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import NavigationBar from './Navigation/NavigationBar';
-import NavigationTabs from "./Navigation/NavigationTabs";
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <NavigationBar />
-        <NavigationTabs />
-      </MuiThemeProvider>
+      <div>
+        Site.js
+      </div>
     );
   }
 }
